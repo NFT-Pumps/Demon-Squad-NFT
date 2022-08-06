705becba25bb3b21a5037ceb19c961d65deb8efe
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-08-06 17:21:11 -0500

    mint

--- scripts/deployArgs.js
+++ scripts/deployArgs.js
@@ -1,24 +1,21 @@
 module.exports = [
- 'Numb Bunny NFT',
- 'NBNFT',
+ 'Demon Squad NFT',
+ 'DSNFT',
  
     '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
     '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
-    'https://public-pre-ipfs.s3.amazonaws.com/NumbBunnyNFT/json/',
-    'https://public-pre-ipfs.s3.amazonaws.com/NumbBunnyNFT/assets/Unrevealed.json',
+    'https://public-pre-ipfs.s3.amazonaws.com/DemonSquadNFT/json/',
+    'https://public-pre-ipfs.s3.amazonaws.com/DemonSquadNFT/assets/Unrevealed.json',
 
     [
-        ethers.utils.getAddress('0xf07839FAa0a863313bf217da333ac4d67aaB5923'),
-        ethers.utils.getAddress('0xF0A1A510191B3563F924C92B76ADD13Dc7CDD78a'),
-        ethers.utils.getAddress('0xa793EBC07b3c44123933c3e4e2b0A7807FEE8257'),
-        ethers.utils.getAddress('0x36CD5Dc24234332B3B7B2DC77AbC57242a05B7c9'),
+        ethers.utils.getAddress('0xdd0D77E16c6F44cB9Aa38891ccae3954DEa8bC2A'),
+        ethers.utils.getAddress('0xD105a0cD7fA1Ad4C9E3af25a0824F2E9aE081782'),
         ethers.utils.getAddress('0x8912e40d291693C7A86C418D52b46766780C6989')
     ],
     [
-        40,
-        35,
-        5,
-        5,
-        15
+        45,
+        45,
+        10
+
     ]
 ]
